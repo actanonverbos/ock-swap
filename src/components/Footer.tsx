7a68cc6a82397f12a4b4b210f1098380c03d2abf
actanonverbos <isaacanthonyx@gmail.com>
2024-09-07 19:19:07 +0800

    Updated footer.tsx to remove unecessary tailwind classes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <section className="mt-auto mb-2 flex w-full flex-col flex-col-reverse justify-between gap-2 md:mt-8 md:mb-6 md:flex-row">
+    <section className="mt-auto mb-2 flex w-full flex-col justify-between gap-2 md:mt-8 md:mb-6 md:flex-row">
       <aside className="flex items-center pt-2 md:pt-0">
         <h3 className="mr-2 mb-2 text-m md:mb-0">
           Built with love by{' '}
